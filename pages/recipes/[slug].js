16a051a8ab35a3aad1a27b7ce552e653949dbd04
nerko85 <nerko_85@live.com>
2021-06-08 14:31:50 +0200

    Adding redirect to homepage

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -104,6 +104,15 @@ export async function getStaticProps({params}){
       content_type:"recipe",
       "fields.slug":params.slug
     });
+
+    if(!items.length) {
+        return {
+            redirect:{
+                destination:'/',
+                permanent:false
+            }
+        }
+    }
   
     return {
       props:{
