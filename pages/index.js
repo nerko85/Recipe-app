a34b528ace2954a2f01aa53f411e21e1d1a404d2
Nermin Bosnjak <nermin@MacBook-Pro.local>
2021-06-13 00:05:29 +0200

    Added category content type

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,74 @@
-import Head from 'next/head'
-import styled from 'styled-components'
-import {createClient} from 'contentful'
-import RecipeCard from '../components/RecipeCard/RecipeCard';
+import Head from "next/head";
+import styled from "styled-components";
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard/RecipeCard";
 
-export default function Home({recipes}) {
+export default function Home({ recipes, categories }) {
+  console.log(categories);
   return (
     <>
       <Head>
         <title>Recipes page</title>
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
 
+      {categories &&
+        categories.map((category) => <div>{category.fields.title}</div>)}
+
       <RecipeList>
-      {recipes.map(recipe=> (
-          <RecipeCard key={recipe.sys.id} recipe={recipe}/>
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.sys.id} recipe={recipe} />
         ))}
       </RecipeList>
     </>
-    )
+  );
 }
 
 const size = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px'
-}
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
 
 const RecipeList = styled.div`
-  display:grid;
-  grid-template-columns:repeat(3, 1fr);
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: 1rem 2rem;
 
-  @media(max-width: ${size.laptopL}){
-    grid-template-columns:repeat(2, 1fr);
+  @media (max-width: ${size.laptopL}) {
+    grid-template-columns: repeat(2, 1fr);
     grid-gap: 1rem 2rem;
   }
 
-  @media(max-width: ${size.tablet}){
+  @media (max-width: ${size.tablet}) {
     grid-template-columns: 1fr;
     grid-gap: 2rem 0;
   }
-`
+`;
 
-export async function getStaticProps(){
-
+export async function getStaticProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
-  const {items} = await client.getEntries({
-    content_type:"recipe"
+  const { items, assets } = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const { items: categories } = await client.getEntries({
+    content_type: "categories",
   });
 
   return {
-    props:{
-      recipes:items
+    props: {
+      recipes: items,
+      categories: categories,
     },
-    revalidate:1
-  }
-
+    revalidate: 1,
+  };
 }
